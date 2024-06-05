f8d2872ceb34fbb715712802aa1d5cb0cac64ae1
Bryan Stitt <bryan@stitthappens.com>
2024-06-04 21:12:39 -0700

    helpers for using viem

--- frontend-leptos/src-js/index.js
+++ frontend-leptos/src-js/index.js
@@ -1,3 +1,43 @@
+import { http, custom, createPublicClient } from 'viem'
+import { arbitrum, base, mainnet } from 'viem/chains'
+
 export function hello() {
   return 'Hello, world!';
 }
+
+export function createPublicClientForChain(chainId) {
+  let chain;
+  switch (chainId) {
+    case "0x1":
+      chain = mainnet
+      break;
+    case "0xa4b1":
+      chain = arbitrum
+      break;
+    case "0x2105":
+      chain = base
+      break;
+    default: throw new Error(`Unsupported chain ID: ${chainId}`)
+  }
+
+  // TODO: maybe instead of just returning the client, we could add an `http()` with a eip6963 event?
+  return createPublicClient({
+    chain,
+    transport: http()
+  });
+};
+
+export function createWalletClientForChain(chainId, eip1193Provider) {
+  let chain;
+  switch (chainId) {
+    case "0xa4b1":
+      chain = arbitrum
+      break;
+    default: throw new Error(`Unsupported chain ID: ${chainId}`)
+  }
+
+  return createWalletClient({
+    chain,
+    transport: custom(eip1193Provider)
+  });
+};
